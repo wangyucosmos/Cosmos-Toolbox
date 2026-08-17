97845db1c9635ef974ce2d6e98a2d9e845593405
cosmoswong <wangyuwangxinxin@gmail.com>
2026-08-17 11:38:50 +0800

    feat: add initial Cosmos OS Xcode project

---
+++ Apps/CosmosOS/Cosmos Toolbox/Cosmos_ToolboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cosmos_ToolboxApp.swift
+//  Cosmos Toolbox
+//
+//  Created by Cosmos on 2026/8/17.
+//
+
+import SwiftUI
+
+@main
+struct Cosmos_ToolboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apps/CosmosOS/Cosmos Toolbox/DashboardView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cosmos Toolbox
+//
+//  Created by Cosmos on 2026/8/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
